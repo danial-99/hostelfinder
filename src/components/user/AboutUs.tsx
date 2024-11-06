146ba3d170c6352c3230b86754a231b5308074b4
danial-99 <alishahaman652@gmail.com>
2024-11-06 15:52:52 +0500

    make changes in the about page

--- src/components/user/AboutUs.tsx
+++ src/components/user/AboutUs.tsx
@@ -9,17 +9,15 @@ export default function AboutUs() {
           <div className='md:w-1/2'>
             <h2 className='text-3xl font-bold mb-4'>About Us</h2>
             <p className='text-gray-600 mb-4'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+            Welcome to <b>HostelFinder</b>, the go-to platform designed to connect students and hostel owners seamlessly and affordably. We understand that finding the perfect accommodation can be a challenge, especially when it comes to balancing quality, safety, and cost. That’s why we created <b>HostelFinder</b> a solution where students and owners can interact directly to find accommodations that meet their needs without the hefty fees associated with traditional services.
             </p>
             <p className='text-gray-600 mb-4'>
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit
-              anim id est laborum.
+            For students, we provide access to verified listings across a range of prices, facilities, and locations. Our platform allows you to compare hostels, view ratings, and read genuine feedback from other students, ensuring a smooth decision-making process. Hostel owners, on the other hand, benefit from a dedicated space to showcase their properties, manage bookings efficiently, and connect with potential residents easily and transparently.
             </p>
+            <p className="text-gray-600 mb-4">
+            Whether you’re searching for a comfortable place to stay during your studies or looking to welcome students to your hostel, <b>HostelFinder</b> offers a trusted, budget-friendly solution tailored for students and owners alike.
+            </p>
+
             <Button>Contact us</Button>
           </div>
           <div className='md:w-1/2'>
